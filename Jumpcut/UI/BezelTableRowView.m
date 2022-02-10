0569e3c2957f37703b8f235586b1872b8be2bc05
schriftgestalt <georg.seifert@mac.com>
2022-02-10 13:28:03 +0100

    add subtle separator

--- Jumpcut/UI/BezelTableRowView.m
+++ Jumpcut/UI/BezelTableRowView.m
@@ -24,5 +24,30 @@
     [bezierPath fill];
 }
 
+- (void)drawSeparatorInRect:(NSRect)dirtyRect
+{
+    if (@available(macOS 11, *)) {
+        [super drawSeparatorInRect:dirtyRect];
+    }
+    else {
+        if (!self.selected) {
+            NSRect frame = self.bounds;
+            [[NSColor tertiaryLabelColor] set];
+            NSBezierPath *bezierpath = [NSBezierPath bezierPathWithRect:NSMakeRect(NSMinX(frame) + 10, NSMaxY(frame) - 1, NSWidth(frame) - 20, 1)];
+
+            NSArray *colors = @[
+                [NSColor clearColor],
+                [[NSColor textColor] colorWithAlphaComponent:0.1],
+                [NSColor clearColor]
+            ];
+            CGFloat locations[3];
+            locations[0] = 0;
+            locations[1] = 0.5;
+            locations[2] = 1;
+            NSGradient *gradient = [[NSGradient alloc] initWithColors:colors atLocations:locations colorSpace:[NSColorSpace sRGBColorSpace]];
+            [gradient drawInBezierPath:bezierpath angle:0];
+        }
+    }
+}
 @end
 
